2d84d4e490a8b7f1c41b2dd872d61901175a3a92
陸安 <andy@luans-MacBook-Air.local>
2021-05-04 18:47:26 +0800

    test groupby

--- w_advance/router/paymentInfo.js
+++ w_advance/router/paymentInfo.js
@@ -10,37 +10,45 @@ router.use(express.static("public"));
 
 router.get("/order/payments", (req, res) => {
     async function calTotal () {
-        const sql = "SELECT user_id, total FROM order_table;";
-        const result = await dbsql(req, sql);
-        // const result = [ // for test
-        //     { user_id: 1, total: 123 },
-        //     { user_id: 2, total: 321 },
-        //     { user_id: 3, total: 111 },
-        //     { user_id: 2, total: 234 }
-        // ];
-        const user = {};
-        const ans = [];
-        // console.log(result);
-        result.forEach(function (ele) {
-            // console.log("================");
-            // console.log(user);
-            if (!user[ele.user_id]) {
-                user[ele.user_id] = { user_id: ele.user_id, total: 0 };
-                ans.push(user[ele.user_id]);
-            }
-            user[ele.user_id].total += ele.total;
-        });
-        // console.log(ans);
-
-        // result.forEach(function (element) { // 另種寫法
-        //     console.log(this[element.user_id]);
-        //     if (!this[element.user_id]) {
-        //         this[element.user_id] = { user_id: element.user_id, total: 0 };
-        //         ans.push(this[element.user_id]);
+        // // =================type 1=================
+        // const sql = "SELECT user_id, total FROM order_table;";
+        // const result = await dbsql(req, sql);
+        // // const result = [ // for test
+        // //     { user_id: 1, total: 123 },
+        // //     { user_id: 2, total: 321 },
+        // //     { user_id: 3, total: 111 },
+        // //     { user_id: 2, total: 234 }
+        // // ];
+        // const user = {};
+        // const ans = [];
+        // // console.log(result);
+        // result.forEach(function (ele) {
+        //     // console.log("================");
+        //     // console.log(user);
+        //     if (!user[ele.user_id]) {
+        //         user[ele.user_id] = { user_id: ele.user_id, total: 0 };
+        //         ans.push(user[ele.user_id]);
         //     }
-        //     this[element.user_id].total += element.total;
-        // }, Object.create(null));
+        //     user[ele.user_id].total += ele.total;
+        // });
+        // // console.log(ans);
 
+        // // result.forEach(function (element) { // 另種寫法
+        // //     console.log(this[element.user_id]);
+        // //     if (!this[element.user_id]) {
+        // //         this[element.user_id] = { user_id: element.user_id, total: 0 };
+        // //         ans.push(this[element.user_id]);
+        // //     }
+        // //     this[element.user_id].total += element.total;
+        // // }, Object.create(null));
+
+        // // =================type 1=================
+
+        // =================type 2=================
+        const sql = "SELECT user_id, SUM(total) FROM stylish.order_table GROUP BY user_id;";
+        const ans = await dbsql(req, sql);
+        // console.log(ans);
+        // =================type 2=================
         const response = {};
         response.data = ans;
         return (response);
