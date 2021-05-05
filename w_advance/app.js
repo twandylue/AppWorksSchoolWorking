1c7747230f72be00c3c527e967af5e2a63c8a647
陸安 <andy@luans-MacBook-Air.local>
2021-05-05 17:51:56 +0800

    test scaling

--- w_advance/app.js
+++ w_advance/app.js
@@ -41,7 +41,7 @@ function DBConnection () {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         waitForConnections: true, // 無可用連線時是否等待pool連線釋放(預設為true)
-        connectionLimit: 60 // 連線池可建立的總連線數上限(預設最多為1500個連線數)
+        connectionLimit: 60 // 連線池可建立的總連線數上限(預設最多為60個連線數)
     });
     // MySQL Connect test
     pool.getConnection((err) => {
